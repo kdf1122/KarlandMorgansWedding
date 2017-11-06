24d37021a576f88b4987e227cc679ac2349f6093
Thomas Fleming <tom@lastcallmedia.com>
2017-11-05 20:44:25 -0500

    Initial commit

---
+++ config/SiteConfig.js
@@ -0,0 +1,26 @@
+module.exports = {
+	pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+	siteTitle: 'Karl & Morgan', // Navigation and Site Title
+	siteTitleAlt: 'Karl & Morgan - July 14, 2018', // Alternative Site title for SEO
+	siteUrl: 'http://www.morganandkarlswedding.xyz', // Domain of your site. No trailing slash!
+	siteLanguage: 'en', // Language Tag on <html> element
+	siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+	siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
+	siteFBAppID: '', // Facebook App ID
+	userTwitter: '', // Twitter Username
+	// og: Meta Tags
+	ogSiteName: '', // Facebook Site Name
+	googleAnalyticsID: '',
+	copyright: 'Copyright © 2017. All rights reserved. Pictures by Unsplash.', // Copyright in the footer of the site
+	// You can translate these three words into your language if you want. They'll be shown on the project page header
+	client: 'Client',
+	date: 'Date',
+	service: 'Service',
+	// Manifest and Progress color
+	themeColor: '#3498DB',
+	backgroundColor: '#2b2e3c',
+	// Settings for typography.js
+	headerFontFamily: 'Merriweather',
+	bodyFontFamily: 'Roboto',
+	baseFontSize: '16px'
+}
